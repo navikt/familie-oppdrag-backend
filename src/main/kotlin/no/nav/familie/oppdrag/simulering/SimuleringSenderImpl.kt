2aa4d01651f84350948a06503e8b615ceaa5218d
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-06-15 12:26:42 +0200

    Fjern @retryable fra Simulering, mangler @EnableRetry

--- src/main/kotlin/no/nav/familie/oppdrag/simulering/SimuleringSenderImpl.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/simulering/SimuleringSenderImpl.kt
@@ -1,12 +1,9 @@
 package no.nav.familie.oppdrag.simulering
 
-import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningFeilUnderBehandling
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningRequest
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningResponse
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerFpService
 import org.springframework.context.annotation.Profile
-import org.springframework.retry.annotation.Backoff
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Profile("never")
@@ -15,7 +12,6 @@ class SimuleringSenderImpl(
     private val port: SimulerFpService,
 ) : SimuleringSender {
     @Override
-    @Retryable(value = [SimulerBeregningFeilUnderBehandling::class], maxAttempts = 3, backoff = Backoff(delay = 4000))
     override fun hentSimulerBeregningResponse(simulerBeregningRequest: SimulerBeregningRequest?): SimulerBeregningResponse =
         port.simulerBeregning(simulerBeregningRequest)
 }
