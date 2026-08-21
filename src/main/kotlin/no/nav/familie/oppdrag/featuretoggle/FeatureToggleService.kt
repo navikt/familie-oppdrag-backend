c04499b9f83d51fb1788ce673aae106d020a4332
bragejahren <brage.ekroll.jahren@nav.no>
2026-08-21 16:04:50 +0200

    Legger til mulighet for å kunne toggle per fagsakId.

--- src/main/kotlin/no/nav/familie/oppdrag/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/featuretoggle/FeatureToggleService.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.oppdrag.featuretoggle
 
+import no.nav.familie.unleash.UnleashContextFields
 import no.nav.familie.unleash.UnleashService
 import org.springframework.stereotype.Service
 
@@ -11,4 +12,10 @@ class FeatureToggleService(
         toggle: FeatureToggle,
         defaultValue: Boolean = false,
     ): Boolean = unleashService.isEnabled(toggle.navn, defaultValue)
+
+    fun isEnabled(toggle: FeatureToggle, defaultValue: Boolean = false, fagsakId: String): Boolean =
+        unleashService.isEnabled(toggle.navn,properties =
+            mapOf(
+                UnleashContextFields.FAGSAK_ID to fagsakId,
+            ),)
 }
