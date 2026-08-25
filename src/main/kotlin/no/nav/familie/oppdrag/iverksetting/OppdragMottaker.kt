f87152fef61d148d4b604d2d4a4260765f37bf42
bragejahren <brage.ekroll.jahren@nav.no>
2026-08-25 09:06:01 +0200

    Erstatter 'never'-profil med 'dev' eller 'preprod' for services relatert til iverksettelse.

--- src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/iverksetting/OppdragMottaker.kt
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Profile("preprod")
+@Profile("dev", "preprod")
 class OppdragMottaker(
     val oppdragLagerRepository: OppdragLagerRepository,
     val env: Environment,
