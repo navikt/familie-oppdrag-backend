0e5b8593c9f3faabf8604d71b3a16cb1a572b153
bragejahren <brage.ekroll.jahren@nav.no>
2026-08-27 10:55:46 +0200

    Legger inn igjen @Profile("preprod") for å sikre at det ikke er mulig å simulere i prod før vi er klare.

--- src/main/kotlin/no/nav/familie/oppdrag/rest/SimuleringController.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/rest/SimuleringController.kt
@@ -11,6 +11,7 @@ import no.nav.familie.oppdrag.simulering.SimuleringTjeneste
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Profile("preprod")
 @RestController
 @RequestMapping(
     "/api/simulering",
